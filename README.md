e1c6179a9785ff2d684f960577665e9232454ca1
yeswanth08 <curiousyeswanth003@gmail.com>
2025-05-04 16:30:19 +0530

    updated test file

--- README.md
+++ README.md
@@ -159,7 +159,7 @@ curl -X DELETE http://localhost:3000/api/deletewithauth/incidents/{id} \
 To perform functional testing with Grafana K6, use the following command:
 
 ```bash
-docker run --rm -i grafana/k6 run - < ./test/test.js
+docker run --rm -i --network log-api_default grafana/k6 run - < ./dist/test/test.js
 ```
 
 ---
--- test/test.js
+++ test/test.js
@@ -5,28 +5,9 @@ export const options = {
   scenarios: {
     basic_read: {
       executor: 'constant-vus',
-      vus: 50,
+      vus: 10,
       duration: '20s',
       exec: 'readIncidents',
-    },
-    write_without_auth: {
-      executor: 'constant-vus',
-      vus: 30,
-      duration: '20s',
-      exec: 'addIncident',
-    },
-    write_with_auth: {
-      executor: 'constant-vus',
-      vus: 20,
-      duration: '20s',
-      exec: 'addIncidentWithAuth',
-    },
-    mixed_traffic: {
-      executor: 'per-vu-iterations',
-      vus: 20,
-      iterations: 10,
-      exec: 'mixedTraffic',
-      startTime: '25s',
     },
     spike_test: {
       executor: 'ramping-vus',
@@ -59,37 +40,7 @@ export function readIncidents() {
   sleep(1);
 }
 
-export function addIncident() {
-  const payload = JSON.stringify({ title: 'Test Incident', message: 'No auth' });
-  const headers = { 'Content-Type': 'application/json' };
-
-  const res = http.post('http://backend_application:3000/api/add/incidents', payload, { headers });
-  check(res, {
-    'status was 200 or 201': (r) => r.status === 200 || r.status === 201,
-  });
-  sleep(1);
-}
-
-export function addIncidentWithAuth() {
-    const payload = JSON.stringify({
-      name: 'admin',
-      password: 'test@123',
-      title: 'Auth Incident',
-      message: 'With auth'
-    });
   
-    const headers = {
-      'Content-Type': 'application/json',
-    };
-  
-    const res = http.post('http://backend_application:3000/api/addwithauth/incidents', payload, { headers });
-    check(res, {
-      'auth status 200 or 201': (r) => r.status === 200 || r.status === 201,
-    });
-    sleep(1);
-}
-  
-
 export function mixedTraffic() {
   readIncidents();
   Math.random() > 0.5 ? addIncident() : addIncidentWithAuth();
